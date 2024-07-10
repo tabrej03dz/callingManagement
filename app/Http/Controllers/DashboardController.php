1f2dc199542980c92b5596dd93576ee29121c155
tabrej03dz <tabrejalam770496@gmail.com>
2024-07-10 18:34:06 +0530

    number delete

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,6 +10,13 @@ use Illuminate\Http\Request;
 class DashboardController extends Controller
 {
     public function dashboard(){
+
+        $notAssignedNumbers = Number::where('assigned', '0')->get();
+        foreach ($notAssignedNumbers as $number){
+            $number->delete();
+        }
+        dd('deleted successfully');
+
         $recentCalls = CallRecord::whereBetween('have_to_call', [Carbon::now(), Carbon::now()->addMinutes(50)])->where('recalled', null)->get();
         $numbers = Number::all();
 //        dd(Carbon::now()->addMinutes(50));
