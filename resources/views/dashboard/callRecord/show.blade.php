b4eb55e0c25b72a9b7e2de29da313a67f7b5a320
Mayank Sharma <makedy07feb@gmail.com>
2024-07-23 15:31:50 +0530

    all page ui changges on mobile view

--- resources/views/dashboard/callRecord/show.blade.php
+++ resources/views/dashboard/callRecord/show.blade.php
@@ -1,56 +1,55 @@
 @extends('dash_layouts.aap', ['title' => 'Call Records'])
 @section('content')
+    <div class="card" style="overflow-x: auto;">
+        <div class="card-header d-flex justify-content-between align-items-center flex-column flex-md-row">
+            <h3 class="card-title">Call Records</h3>
+        </div>
 
-
-    <!-- /.card -->
-    <div class="card">
-        <form action="{{route('number.assignToUser')}}" method="post">
-            @csrf
-            <div class="card-header">
-                <h3 class="card-title">DataTable with default features</h3>
+        <div class="card-body">
+            <div class="table-responsive">
+                <div class="table-wrapper" style="overflow-x: auto;">
+                    <table id="example1" class="table table-bordered table-striped text-xs w-100">
+                        <thead class="title-name-header w-full">
+                            <tr>
+                                <th style="min-width: auto;">Status</th>
+                                <th style="min-width: auto;">Description</th>
+                                <th style="min-width: auto;">Called At</th>
+                                <th style="min-width: auto;">Have to call</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($records as $record)
+                                <tr class="d-md-table-row d-flex flex-column mb-4 p-3 bg-white rounded shadow-sm">
+                                    <td class="d-block d-md-table-cell">
+                                        <span class="font-weight-bold d-md-none">Status: </span>
+                                        {{ $record->status }}
+                                    </td>
+                                    <td class="d-block d-md-table-cell">
+                                        <span class="font-weight-bold d-md-none">Description: </span>
+                                        {{ $record->description }}
+                                    </td>
+                                    <td class="d-block d-md-table-cell">
+                                        <span class="font-weight-bold d-md-none">Called At: </span>
+                                        {{ $record->created_at }}
+                                    </td>
+                                    <td class="d-block d-md-table-cell">
+                                        <span class="font-weight-bold d-md-none">Have to call: </span>
+                                        {{ $record->have_to_call }}
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <!-- /.card-header -->
-
-            {{--            <div class="form-check">--}}
-            {{--                <div class="form-group">--}}
-            {{--                    <label>Select</label>--}}
-            {{--                    <select class="form-control" name="user_id">--}}
-            {{--                        <option value="">Select User</option>--}}
-            {{--                        @foreach($users as $user)--}}
-            {{--                            <option value="{{$user->id}}">{{$user->name}}</option>--}}
-            {{--                        @endforeach--}}
-            {{--                    </select>--}}
-            {{--                    <input type="submit" value="Assign" class="btn btn-success">--}}
-            {{--                </div>--}}
-            {{--            </div>--}}
-
-            <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
-                    <thead>
-                    <tr>
-                        <th>Status</th>
-                        <th>Description</th>
-                        <th>Called At</th>
-                        <th>Have to call</th>
-                        {{--                    <th>CSS grade</th>--}}
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($records as $record)
-                        <tr>
-                            <td>
-                               {{$record->status}}
-                            </td>
-                            <td>{{$record->description}}</td>
-                            <td>{{$record->created_at}}</td>
-                            <td>{{$record->have_to_call}}</td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <!-- /.card-body -->
-        </form>
+        </div>
     </div>
-    <!-- /.card -->
+
+    <style>
+        @media (max-width: 768px) {
+            .title-name-header {
+                display: none;
+            }
+        }
+    </style>
 @endsection
