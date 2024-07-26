92976b03999a936357d38bb7afc4b8317160b8a3
tabrej03dz <tabrejalam770496@gmail.com>
2024-07-26 14:57:37 +0530

    issues fixed

--- resources/views/dashboard/number/assigned.blade.php
+++ resources/views/dashboard/number/assigned.blade.php
@@ -350,8 +350,8 @@
                                                             </div>
 
                                                             <div class="form-group mb-3">
-                            <textarea name="description" id="description" cols="30" rows="5" class="form-control"
-                                      placeholder="Description" style="resize: none;">{{ old('description') }}</textarea>
+                                                                <textarea name="description" id="description" cols="30" rows="5" class="form-control"
+                                                                          placeholder="Description" style="resize: none;">{{ old('description') }}</textarea>
                                                             </div>
                                                             <div class="form-group mb-3">
                                                                 <label for="">Call back time</label>
@@ -373,7 +373,34 @@
                                             </div>
 
                                         </div>
+                                        <a href="{{ route('callRecord.show', ['number' => $number->id]) }}"
+                                           class="btn btn-primary btn-sm">Records</a>
                                     </div>
+
+
+                                </td>
+                                <td class="d-block d-md-table-cell" style="min-width: 300px;">
+                                    <!-- Adjusted min-width for Demo -->
+                                    <span class="font-weight-bold d-md-none">Demo: </span>
+                                    <form action="{{ route('demo.send', ['number' => $number->id]) }}"
+                                          class="form-inline" method="post">
+                                        @csrf
+                                        <div class="btn-group w-100">
+                                            <div class="d-flex flex-column flex-md-row w-100 align-items-center">
+                                                <select name="demo_id"
+                                                        class="form-control form-control-sm mb-2 mb-md-0 mr-md-2 w-32 h-12">
+                                                    <option value="">Select Demo</option>
+                                                    @foreach ($demos as $demo)
+                                                        <option value="{{ $demo->id }}">
+                                                            {{ $demo->name . ' - ' . $demo->city }}
+                                                        </option>
+                                                    @endforeach
+                                                </select>
+                                                <textarea id="autoResizeTextarea" name="custom_message" class="form-control form-control-sm mb-2 mb-md-0 mr-md-2 flex-grow-1 w-32" placeholder="Custom Message"></textarea>
+                                                <button type="submit"
+                                                        class="btn btn-primary btn-sm lg:w-32 md:w-32 h-12 mt-2 mt-md-0 px-3">Send</button>
+                                            </div>
+                                        </div>
                                     </form>
                                 </td>
                             </tr>
@@ -613,74 +640,6 @@
     </div>
 
 
-    <!-- Modal -->
-    {{--    <div class="modal fade" id="responseModal" tabindex="-1" role="dialog" aria-labelledby="responseModalLabel" --}}
-    {{--        aria-hidden="true"> --}}
-    {{--        <div class="modal-dialog" role="document"> --}}
-    {{--            <div class="modal-content"> --}}
-    {{--                <div class="modal-header"> --}}
-    {{--                    <h5 class="modal-title" id="responseModalLabel">Response Form</h5> --}}
-    {{--                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"> --}}
-    {{--                        <span aria-hidden="true">&times;</span> --}}
-    {{--                    </button> --}}
-    {{--                </div> --}}
-    {{--                <div class="modal-body"> --}}
-    {{--                    <form role="form" action="{{ route('callRecord.store', ['number' => $number->id]) }}" --}}
-    {{--                        method="post"> --}}
-    {{--                        @csrf --}}
-
-    {{--                        <div class="form-group mb-3"> --}}
-    {{--                            <select name="status" class="form-control custom-select"> --}}
-    {{--                                <option value="">Response</option> --}}
-    {{--                                <option value="call pick" {{ old('status') == 'call pick' ? 'selected' : '' }}>Call Pick --}}
-    {{--                                </option> --}}
-    {{--                                <option value="call not pick" {{ old('status') == 'call not pick' ? 'selected' : '' }}> --}}
-    {{--                                    Call Not Pick</option> --}}
-    {{--                                <option value="call back" {{ old('status') == 'call back' ? 'selected' : '' }}>Call Back --}}
-    {{--                                </option> --}}
-    {{--                            </select> --}}
-    {{--                        </div> --}}
-
-    {{--                        <div class="form-group mb-3"> --}}
-    {{--                            <select name="number_status" class="form-control custom-select"> --}}
-    {{--                                <option value="">Number Status</option> --}}
-    {{--                                <option value="interested" {{ old('number_status') == 'interested' ? 'selected' : '' }}> --}}
-    {{--                                    Interested</option> --}}
-    {{--                                <option value="not interested" --}}
-    {{--                                    {{ old('number_status') == 'not interested' ? 'selected' : '' }}>Not Interested --}}
-    {{--                                </option> --}}
-    {{--                                <option value="wrong number" --}}
-    {{--                                    {{ old('number_status') == 'wrong number' ? 'selected' : '' }}>Wrong Number</option> --}}
-    {{--                                <option value="converted" {{ old('number_status') == 'converted' ? 'selected' : '' }}> --}}
-    {{--                                    Converted</option> --}}
-    {{--                            </select> --}}
-    {{--                        </div> --}}
-
-    {{--                        <div class="form-group mb-3"> --}}
-    {{--                            <textarea name="description" id="description" cols="30" rows="5" class="form-control" --}}
-    {{--                                placeholder="Description" style="resize: none;">{{ old('description') }}</textarea> --}}
-    {{--                        </div> --}}
-    {{--                        <div class="form-group mb-3"> --}}
-    {{--                            <label for="">Call back time</label> --}}
-    {{--                            <input name="date_and_time" type="datetime-local" class="form-control" --}}
-    {{--                                placeholder="Have to call" value="{{ old('date_and_time') }}" /> --}}
-    {{--                        </div> --}}
-
-    {{--                        <div class="form-group mb-3"> --}}
-    {{--                            <input name="send_message" id="send_message" type="checkbox" value="true" /> --}}
-    {{--                            <label for="send_message">Do you want to send message to this number</label> --}}
-    {{--                        </div> --}}
-
-    {{--                        <div class="form-group mb-3"> --}}
-    {{--                            <button type="submit" class="btn btn-primary">Submit</button> --}}
-    {{--                        </div> --}}
-    {{--                    </form> --}}
-    {{--                </div> --}}
-    {{--            </div> --}}
-    {{--        </div> --}}
-    {{--    </div> --}}
-
-
     <style>
         .highlighted-row {
             background-color: #ffffcc !important;
@@ -777,7 +736,7 @@
             console.log('Saved Number ID:', savedNumberId);
 
             if (savedNumberId) {
-                const row = document.getElementById(row-${savedNumberId});
+                const row = document.getElementById(`row-${savedNumberId}`);
                 console.log('Row Element:', row);
 
                 if (row) {
